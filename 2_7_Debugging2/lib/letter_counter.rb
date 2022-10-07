68a3a2ecc6299098af32317a052ae9858c09fa0b
Gavin Moorcroft <gavin.moorcroft1@gmail.com>
2022-10-07 17:02:37 +0100

    add debig challenge code

---
+++ 2_7_Debugging2/lib/letter_counter.rb
@@ -0,0 +1,67 @@
+
+## challenge is to debug the following code:
+
+# class LetterCounter
+#     def initialize(text)
+#       @text = text
+#     end
+  
+#     def calculate_most_common()
+#       counter = Hash.new(1)
+#       most_common = nil
+#       most_common_count = 1
+#       @text.chars.each do |char|
+#         next unless is_letter?(char)
+#         counter[char] = (counter[char] || 1) + 1
+#         if counter[char] > most_common_count
+#           most_common = char
+#           most_common_count += counter[char]
+#         end
+#       end
+#       return [most_common_count, most_common]
+#     end
+  
+#     private
+  
+#     def is_letter?(letter)
+#       return letter =~ /[a-z]/i
+#     end
+#   end
+  
+
+  
+## Intended output:
+## [2, "i"]
+
+
+class LetterCounter
+    def initialize(text)
+      @text = text
+    end
+  
+    def calculate_most_common()
+      counter = Hash.new(0)
+      most_common = nil
+      most_common_count = 0
+      @text.chars.each do |char|
+        next unless is_letter?(char)
+        counter[char] = (counter[char] || 1 ) + 1
+        if counter[char] > most_common_count
+          most_common = char
+          most_common_count = counter[char]
+        end
+      end
+      return [most_common_count, most_common]      
+    end
+  
+    private
+  
+    def is_letter?(letter)
+      return letter =~ /[a-z]/i 
+    end
+end
+  
+  counter = LetterCounter.new("abbcaddabra")
+  p counter.calculate_most_common
+
+  
