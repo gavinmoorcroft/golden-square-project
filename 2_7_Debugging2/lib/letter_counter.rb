eb97f756f1cd08f27c8d6c948f71f03e77c8a2d8
Gavin Moorcroft <gavin.moorcroft1@gmail.com>
2022-10-11 09:20:53 +0100

    add commments of changes

--- 2_7_Debugging2/lib/letter_counter.rb
+++ 2_7_Debugging2/lib/letter_counter.rb
@@ -40,14 +40,17 @@ class LetterCounter
     end
   
     def calculate_most_common()
+      #counter = Hash.new(1)
       counter = Hash.new(0)
       most_common = nil
+      #most_common_count = 1
       most_common_count = 0
       @text.chars.each do |char|
         next unless is_letter?(char)
         counter[char] = (counter[char] || 1 ) + 1
         if counter[char] > most_common_count
           most_common = char
+          #most_common_count += counter[char]
           most_common_count = counter[char]
         end
       end
@@ -61,7 +64,7 @@ class LetterCounter
     end
 end
   
-  counter = LetterCounter.new("abbcaddabra")
+  counter = LetterCounter.new("Digital Punk")
   p counter.calculate_most_common
 
   
